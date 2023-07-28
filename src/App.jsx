3980a3b790dbc90bf1edfbe1420051e3a66c8bb4
shmaphilo <philipogaye062@gmail.com>
2023-07-28 13:09:27 -0400

    Assignment done

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import TransactionList from "./TransactionList";
+import TransactionForm from "./TransactionForm";
+
+const App=()=>{
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  const fetchTransactions = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/transactions"); 
+      if (response.ok) {
+        const data = await response.json();
+        setTransactions(data);
+      } else {
+        console.error("Error fetching transactions:", response.status);
+      }
+    } catch (error) {
+      console.error("Error fetching transactions:", error);
+    }
+  };
+
+  const addTransaction = (newTransaction) => {
+    setTransactions([...transactions, newTransaction]);
+  };
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredTransactions = transactions.filter((transaction) =>
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>Bank Transactions</h1>
+      <TransactionForm addTransaction={addTransaction} />
+      <input
+        type="text"
+        placeholder="Search by description"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <TransactionList transactions={filteredTransactions} />
+    </div>
+  );
+};
+
+export default App;
